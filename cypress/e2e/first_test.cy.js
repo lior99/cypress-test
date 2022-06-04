c68f1deff2ce582040b317c56bda59e5347654d2
Lior Shtivelman <lior.shtivelman@gmail.com>
2022-06-04 10:16:12 +0300

    Added eslint

--- cypress/e2e/first_test.cy.js
+++ cypress/e2e/first_test.cy.js
@@ -1,27 +1,27 @@
-const { log } = console;
-
+/* eslint-disable no-undef */
+// eslint-disable-next-line no-undef
 describe('Testing the react app', () => {
-    beforeEach(() => {
-      cy.visit('http://localhost:3000')
-    })
-  
-    it('should have 3 todo list items', () => {
-      cy.get('ul li').should('have.length', 3)
-    })
+  // eslint-disable-next-line no-undef
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  });
 
-    it('should have one todo item completed', () => {
-        cy.get('ul li').children().should('have.class', 'complete');
-    })
+  it('should have 3 todo list items', () => {
+    cy.get('ul li').should('have.length', 3);
+  });
 
-    it('should mark one of the items as completed', () => {
-      cy.get('ul li').within(elementes => {
-        const liItem = elementes[1];
-        const checkbox = liItem.querySelector('input[type=checkbox]');
-        checkbox.click();
+  it('should have one todo item completed', () => {
+    cy.get('ul li').children().should('have.class', 'complete');
+  });
 
-        const label = liItem.querySelector('label');
-        return cy.get(label).should('have.class', 'complete')
-      })
-    })
-  })
-  
+  it('should mark one of the items as completed', () => {
+    cy.get('ul li').within((elementes) => {
+      const liItem = elementes[1];
+      const checkbox = liItem.querySelector('input[type=checkbox]');
+      checkbox.click();
+
+      const label = liItem.querySelector('label');
+      return cy.get(label).should('have.class', 'complete');
+    });
+  });
+});
